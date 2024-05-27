eea594bf67f947442005133df6ea7047ee96068c
Waleed Afzal <144891433+beastwaleed@users.noreply.github.com>
2024-05-27 13:10:40 +0500

    Add files via upload

---
+++ ++PostfixOperatorOverload.cpp
@@ -0,0 +1,30 @@
+//////// Operating ++ Postfix Operator for Objects /////////
+#include<iostream>
+using namespace std;
+
+class count{
+    private:
+    int n;
+
+    public:
+
+    count(){
+        n=2;
+    }
+
+    void operator ++ (int){
+        n++;
+    }
+
+    void show(){
+        cout<<"N = "<<n<<endl;
+    }
+};
+
+int main(){
+    count obj;
+    obj.show();
+    obj++;
+    obj.show();
+
+}
